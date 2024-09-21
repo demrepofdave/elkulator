fcc64368031a1867bfc52ae2dd0544ec36dc3066
DemRepOfDave <demrepofdave@bishop-morrison.com>
2024-09-21 13:12:37 +0100

    demrepofdave/al5int/implement basic pipelines (#14)
    * demrepofdave/al5init/implement_basic_pipelines: Initial pipeline added
    
    * demrepofdave/al5init/implement_basic_pipelines: Corrected workflows directory placement
    
    * demrepofdave/al5init/implement_basic_pipelines: Added scripts directory for ci and user builds

---
+++ scripts/autoconfigure.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+# Run automake/autoconf tools + create automake links.
+aclocal -I m4
+automake -a
+autoconf
+
+# Run the project configuration script produced by autoconf.
+./configure $1
